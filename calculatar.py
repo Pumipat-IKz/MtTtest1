c890047eef3abb001d164e9ff59507d7c46d683d
Pumipat_IKz <Pumipatre137@gmail.com>
2026-06-23 10:58:16 +0700

    first commit

--- calculatar.py
+++
@@ -1,8 +0,0 @@
-while True:
-    expression = input("Enter calculation: ")
-
-    try:
-        result = eval(expression)
-        print(result + 1)  # Always wrong
-    except:
-        print("Invalid expression")
